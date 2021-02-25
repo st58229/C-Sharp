3d5aa5eb7627bc0e0284d9d5e015541d8b12d758
st58229 <72087858+st58229@users.noreply.github.com>
2021-02-25 10:14:45 +0100

    Add files via upload

---
+++ Cviceni_02/BaseLib/Reading.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace fei
+{
+    namespace BaseLib
+    {
+        /// <summary>
+        /// Class for reading/writing operations
+        /// </summary>
+        public class Reading
+        {
+            /// <summary>
+            /// Reading and writing Int from/to console
+            /// </summary>            
+            /// <return>
+            /// Value of user input (int)
+            /// </return>
+            public static int ReadInt()
+            {
+                Console.Write("Zapiště libovolné číslo formátu int: ");                
+                int inp; 
+                int.TryParse(Console.ReadLine(), out inp);
+                //Console.WriteLine("Zadali jste číslo: {0}", inp);
+                return inp;
+            }
+            /// <summary>
+            /// Reading and writing Double from/to console
+            /// </summary>
+            /// <return>
+            /// Value of user input (double)
+            /// </return>
+            public static double ReadDouble()
+            {
+                Console.Write("Zapiště libovolné číslo formátu double: ");
+                double inp = Convert.ToDouble(Console.ReadLine());
+                //Console.WriteLine("Zadali jste číslo: {0}", inp);
+                return inp;
+            }
+            /// <summary>
+            /// Reading and writing Char from/to console
+            /// </summary>
+            /// <return>
+            /// Value of user input (char)
+            /// </return>
+            public static char ReadChar()
+            {
+                Console.Write("Zapiště libovolný jeden char: ");
+                char inp = Console.ReadKey().KeyChar;
+                //Console.WriteLine();
+                //Console.WriteLine("Zadali jste char: '{0}'", inp);
+                return inp;
+            }
+            /// <summary>
+            /// Reading and writing String from/to console
+            /// </summary>        
+            /// <return>
+            /// Value of user input (string)
+            /// </return>
+            public static string ReadString()
+            {
+                Console.Write("Zapiště libovolný text: ");
+                string inp = Console.ReadLine();
+                //Console.WriteLine("Zadali jste číslo: \"{0}\"", inp);
+                return inp;
+            }
+        }
+    }
+}
---
+++ Cviceni_02/Cshrp_CV02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using fei.BaseLib;
+
+namespace Cshrp_CV02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Reading.ReadChar();
+            Console.WriteLine();
+            Reading.ReadDouble();
+            Console.WriteLine();
+            Reading.ReadInt();
+            Console.WriteLine();
+            Reading.ReadString();*/
+
+            /*Field pole = new Field(10);
+            pole.add(15);
+            pole.add(45);
+            pole.add(45);
+            pole.add(45);
+            pole.add(45);
+            pole.writeToConsole();
+            pole.order(Field.Order.ACS);
+            pole.order(Field.Order.DES);
+            pole.min();
+            pole.max();
+            pole.firstIndexOfNumber(45);
+            pole.lastIndexOfNumber(45);*/
+
+            Field pole = new Field(5);
+            pole.menuUI();
+        }
+    }
+}
